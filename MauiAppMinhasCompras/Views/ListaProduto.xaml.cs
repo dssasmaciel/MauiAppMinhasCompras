054a34de133626947a09d2221d5456d2fc269047
dssasmaciel <ds.sasmaciel@gmail.com>
2025-08-16 23:13:20 -0300

    Inserção dos botões somar e adicionar / manipulação ListaProduto.xaml e NovoProduto.xaml

--- MauiAppMinhasCompras/Views/ListaProduto.xaml.cs
+++ MauiAppMinhasCompras/Views/ListaProduto.xaml.cs
@@ -4,6 +4,19 @@ public partial class ListaProduto : ContentPage
 {
 	public ListaProduto()
 	{
-		InitializeComponent();
+        InitializeComponent();
 	}
+
+    private void ToolbarItem_Clicked(object sender, EventArgs e)
+    {
+        try
+        {
+            Navigation.PushAsync(new Views.NovoProduto());
+
+        }catch (Exception ex)
+        {
+            DisplayAlert("Ops", ex.Message, "OK");
+        }
+
+    }
 }
