9062ec107eec73d671c4883e676c046cc75fa129
dssasmaciel <ds.sasmaciel@gmail.com>
2025-09-06 17:59:49 -0300

    Finalização do básico do aplicativo = remover, atualizar e selecionar itens da lista. Melhoria= validação da entrada do usuário

--- MauiAppMinhasCompras/Views/EditarProduto.xaml.cs
+++ MauiAppMinhasCompras/Views/EditarProduto.xaml.cs
@@ -1,3 +1,5 @@
+using MauiAppMinhasCompras.Models;
+
 namespace MauiAppMinhasCompras.Views;
 
 public partial class EditarProduto : ContentPage
@@ -6,4 +8,45 @@ public partial class EditarProduto : ContentPage
 	{
 		InitializeComponent();
 	}
+
+    private async void ToolbarItem_Clicked(object sender, EventArgs e)
+    {
+        try
+        {
+            if (string.IsNullOrWhiteSpace(txt_descricao.Text) ||
+                string.IsNullOrWhiteSpace(txt_quantidade.Text) ||
+                string.IsNullOrWhiteSpace(txt_preco.Text))
+            {
+                await DisplayAlert("Erro", "Preencha todos os campos.", "OK");
+                return;
+            }
+
+            if (double.TryParse(txt_quantidade.Text, out double quantidade) &&
+                double.TryParse(txt_preco.Text, out double preco))
+            {
+             Produto produto_anexado = BindingContext as Produto;
+
+             Produto p = new Produto
+            {
+                Id = produto_anexado.Id,
+                Descricao = txt_descricao.Text,
+                Quantidade = quantidade,
+                Preco = preco
+            };
+
+            await App.Db.Update(p);   
+            await DisplayAlert("Sucesso!", "Registro Atualizado", "OK");
+            await Navigation.PopAsync();
+
+        }
+            else
+            {
+                await DisplayAlert("Erro de conversão", "A quantidade e o preço devem ser números válidos.", "OK");
+            }
+        }
+        catch (Exception ex)
+        {
+            await DisplayAlert("Ops", ex.Message, "OK");
+        }
+    }
 }
